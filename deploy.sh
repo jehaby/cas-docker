a81ba978cd0d3545b64b9563a6cf8795f392387c
Stas Makarov <makarov.stanislav@220-volt.ru>
2016-04-22 18:48:32 +0300

    Added scripts with parameters

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# variables defined in .env will be exported into this script's environment:
+set -a
+source .env
+
+# Let's delete all containers that have a `my-project_` prefix:
+# (by deleting/re-deploying our containers, this script is now idempotent!):
+
+docker rm -f `docker ps -aq -f name=$PROJECT_*`
+
+# Now lets populate our nginx config templates to get an actual nginx config
+# (which will be loaded into our nginx container):
+
+envsubst '$CAS_DOMAINS' < configs/cas.conf.tpl > configs/cas.conf
+envsubst '$MY_DOMAINS' < configs/my.conf.tpl > configs/my.conf
+
+# Let's populate the variables in our compose file template,
+# then deploy it!
+cat compose-template.yml | envsubst | docker-compose -f - -p my-project_ up -d
