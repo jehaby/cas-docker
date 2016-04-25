0511abb028308f0885e2633ec79fe87babc270d0
Stas Makarov <makarov.stanislav@220-volt.ru>
2016-04-25 16:16:32 +0300

    MS-45 added ip check

--- deploy.sh
+++ deploy.sh
@@ -1,23 +1,26 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # variables defined in .env will be exported into this script's environment:
 set -a
 source .env
 
+get_my_ip() {
+    echo $(ip -o -4 addr list ${1:-eth0} | awk '{print $4}' | cut -d/ -f1)
+}
+
+HOST_IP=$(get_my_ip)
+
 # Let's delete all containers that have a `my-project_` prefix:
 # (by deleting/re-deploying our containers, this script is now idempotent!):
-
 rm docker-compose.yml
 docker rm -f `docker ps -aq -f name=$PROJECT_*`
 
 # Now lets populate our nginx config templates to get an actual nginx config
 # (which will be loaded into our nginx container):
-
 envsubst '$CAS_DOMAINS' < configs/cas.conf.tpl > configs/cas.conf
 envsubst '$MY_DOMAINS' < configs/my.conf.tpl > configs/my.conf
 
 # Let's populate the variables in our compose file template,
 # then deploy it!
-
 cat compose-template.yml | envsubst > docker-compose.yml
 docker-compose -p my-project_ up
