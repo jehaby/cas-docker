6d82bf2df71e864ef8ead133c1c0b4d0b7d9827d
Stas Makarov <makarov.stanislav@220-volt.ru>
2016-04-25 18:47:29 +0300

    Working extensions

--- deploy.sh
+++ deploy.sh
@@ -12,13 +12,12 @@ HOST_IP=$(get_my_ip)
 
 # Let's delete all containers that have a `my-project_` prefix:
 # (by deleting/re-deploying our containers, this script is now idempotent!):
-rm docker-compose.yml
 docker rm -f `docker ps -aq -f name=$PROJECT_*`
 
 # Now lets populate our nginx config templates to get an actual nginx config
 # (which will be loaded into our nginx container):
-envsubst '$CAS_DOMAINS' < configs/cas.conf.tpl > configs/cas.conf
-envsubst '$MY_DOMAINS' < configs/my.conf.tpl > configs/my.conf
+envsubst '$CAS_DOMAINS:$PROJECT' < configs/cas.conf.tpl > configs/cas.conf
+envsubst '$MY_DOMAINS:$PROJECT' < configs/my.conf.tpl > configs/my.conf
 
 # Let's populate the variables in our compose file template,
 # then deploy it!
