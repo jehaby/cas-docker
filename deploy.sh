15449120b008d153dea025ce3145fa726e39287f
Stas Makarov <makarov.stanislav@220-volt.ru>
2016-04-25 15:29:06 +0300

    Refactoring

--- deploy.sh
+++ deploy.sh
@@ -7,6 +7,7 @@ source .env
 # Let's delete all containers that have a `my-project_` prefix:
 # (by deleting/re-deploying our containers, this script is now idempotent!):
 
+rm docker-compose.yml
 docker rm -f `docker ps -aq -f name=$PROJECT_*`
 
 # Now lets populate our nginx config templates to get an actual nginx config
@@ -17,4 +18,6 @@ envsubst '$MY_DOMAINS' < configs/my.conf.tpl > configs/my.conf
 
 # Let's populate the variables in our compose file template,
 # then deploy it!
-cat compose-template.yml | envsubst | docker-compose -f - -p my-project_ up -d
+
+cat compose-template.yml | envsubst > docker-compose.yml
+docker-compose -p my-project_ up
